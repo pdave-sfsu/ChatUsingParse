55fd7b0ad003e6fabc8228e0b4aeabbd8c06d2b8
PD <dave.sfsu@gmail.com>
2017-02-22 21:55:34 -0800

    Added the tableView and cell and got everything working. Now I have to clean it up

--- ChatUsingParse/ChatViewController.swift
+++ ChatUsingParse/ChatViewController.swift
@@ -9,14 +9,51 @@
 import UIKit
 import Parse
 
-class ChatViewController: UIViewController {
+class ChatViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet weak var messageTextField: UITextField!
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var objects: [PFObject]!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.estimatedRowHeight = 100
+        tableView.rowHeight = UITableViewAutomaticDimension
+        
+        Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ChatViewController.onTimer), userInfo: nil, repeats: true)
+
+        
         // Do any additional setup after loading the view.
+    }
+    
+    func onTimer () {
+        let query = PFQuery(className:"Message")
+        query.whereKeyExists("text")
+        query.findObjectsInBackground { (objects: [PFObject]?, error: Error?) in
+            if error == nil {
+                // The find succeeded.
+                print("Successfully retrieved \(objects!.count) messages.")
+                // Do something with the found objects
+                if let objects = objects {
+                    self.objects = objects
+                    for object in objects {
+                        print(object.objectId!)
+                    }
+                }
+            } else {
+                // Log details of the failure
+                print("Error: \(error!)")
+            }
+
+        }
+        
+        self.tableView.reloadData()
+    
     }
 
     @IBAction func sendMessage(_ sender: Any) {
@@ -33,6 +70,33 @@ class ChatViewController: UIViewController {
         }
 
         
+        
+    }
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if let objects = objects {
+            return objects.count
+        } else {
+            return 0
+        }
+        
+        
+    }
+    
+    
+    // Row display. Implementers should *always* try to reuse cells by setting each cell's reuseIdentifier and querying for available reusable cells with dequeueReusableCellWithIdentifier:
+    // Cell gets various attributes set automatically based on table (separators) and data source (accessory views, editing controls)
+    
+    @available(iOS 2.0, *)
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "messageCell", for: indexPath) as! MessageCell
+        
+        let object = objects[indexPath.row]
+        
+        cell.messageLabel.text = object["text"] as! String?
+        
+        return cell
         
     }
     
---
+++ ChatUsingParse/MessageCell.swift
@@ -0,0 +1,27 @@
+//
+//  MessageCell.swift
+//  ChatUsingParse
+//
+//  Created by Poojan Dave on 2/22/17.
+//  Copyright © 2017 Poojan Dave. All rights reserved.
+//
+
+import UIKit
+
+class MessageCell: UITableViewCell {
+
+    @IBOutlet weak var messageLabel: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
